61503ac091c144f9b259760c88939e6bc07637cc
Natalia <nataliya_nedbaylo@yahoo.com>
2022-10-25 21:56:02 +0200

    markup for About us page added;

--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,8 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
-    'items' => ['ItemController', 'index',],
+    'about' => ['HomeController', 'about'],
+    'items' => ['ItemController', 'index',],    
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
