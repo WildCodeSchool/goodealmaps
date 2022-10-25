c4c0b95a8a078c74bd9c163eb3fb29bd5f4e598f
Franck <depoorter.franck@gmail.com>
2022-10-25 15:22:09 +0200

    Css mobile and desktop map , and start working column left

--- public/assets/javascript/modalMap.js
+++ public/assets/javascript/modalMap.js
@@ -1,38 +1,11 @@
-// const modal = document.querySelector(".modalMap");
-// const trigger = document.querySelector(".mapBtn");
-// const closeButton = document.querySelector(".closeButtonMap");
-
-// function toggleModalMap() {
-//   modal.classList.toggle("showModal");
-// }
-
-// function windowOnClick(event) {
-//   if (event.target === modal) {
-//     toggleModalMap();
-//   }
-// }
-
-// trigger.addEventListener("click", toggleModalMap);
-// closeButton.addEventListener("click", toggleModalMap);
-// window.addEventListener("click", windowOnClick);
-
 
 const modal = document.querySelector(".modalMap");
 const modalNAQ = document.querySelector(".modalMap");
+const areas = document.getElementsByClassName("area");
 
-const GES = document.querySelector(".mapBtnGES");
-const NAQ= document.querySelector(".mapBtnNAQ");
-const ARA= document.querySelector(".mapBtnARA");
-const BFC= document.querySelector(".mapBtnBFC");
-const BRE= document.querySelector(".mapBtnBRE");
-const CVL= document.querySelector(".mapBtnCVL");
-const COR= document.querySelector(".mapBtnCOR");
-const IDF= document.querySelector(".mapBtnIDF");
-const OCC= document.querySelector(".mapBtnOCC");
-const HDF= document.querySelector(".mapBtnHDF");
-const NOR= document.querySelector(".mapBtnNOR");
-const PDL= document.querySelector(".mapBtnPDL");
-const PAC= document.querySelector(".mapBtnPAC");
+Array.from(areas).map((area) => {
+  area.addEventListener("click", toggleModal);
+});
 
 const closeButton = document.querySelector(".closeButtonMap");
 
@@ -46,23 +19,6 @@ function windowOnClick(event) {
   }
 }
 
-GES.addEventListener("click", toggleModal);
-NAQ.addEventListener("click", toggleModal);
-ARA.addEventListener("click", toggleModal);
-BFC.addEventListener("click", toggleModal);
-BRE.addEventListener("click", toggleModal);
-CVL.addEventListener("click", toggleModal);
-COR.addEventListener("click", toggleModal);
-IDF.addEventListener("click", toggleModal);
-OCC.addEventListener("click", toggleModal);
-HDF.addEventListener("click", toggleModal);
-NOR.addEventListener("click", toggleModal);
-PDL.addEventListener("click", toggleModal);
-PAC.addEventListener("click", toggleModal);
-
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
-
-
-
 
