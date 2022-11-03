b04fed402c4da93f86dd0e70b70c1b89d14a6ea8
Natalia <nataliya_nedbaylo@yahoo.com>
2022-11-03 19:36:15 +0100

    code quality error fixed;

--- src/Model/AboutManager.php
+++ src/Model/AboutManager.php
@@ -7,14 +7,13 @@ use PDO;
 class AboutManager extends AbstractManager
 {
     public const TABLE = 'devs';
-    
 
     /**
      * return devs info
      */
     public function renderDevs(): array
-    {        
-        $statement = $this->pdo->prepare("SELECT * FROM `" . self::TABLE . "`");        
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM `" . self::TABLE . "`");
         $statement->execute();
         return $statement->fetchAll();
     }
