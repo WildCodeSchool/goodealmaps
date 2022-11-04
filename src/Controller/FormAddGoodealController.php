3f38a206af251c4e41a4207793df0fc73875fe67
Axel Juste <ax.juste@gmail.com>
2022-11-04 14:21:16 +0100

    form update

--- src/Controller/FormAddGoodealController.php
+++ src/Controller/FormAddGoodealController.php
@@ -16,42 +16,39 @@ class FormAddGoodealController extends AbstractController
         $errors = [];
         $gooDeal = [];
 
-        foreach($data as $key => $value){
+        foreach ($data as $key => $value) {
             $cleanValue = htmlentities(trim($value));
-            if($cleanValue === ""){
+            if ($cleanValue === "") {
                 $errors[$key] = "Merci de Remplir le champs $key";
             }
-            if($key === "title" && strlen($cleanValue) > 50){
+          /*  if ($key === "title" && strlen($cleanValue) > 50) {
                 $errors[$key] = "Le nom du Goodeal est trop long, merci de donner un nom plus court à votre Goodeal";
             }
-            if($key === "message" && strlen($cleanValue) > 65535){
+            if ($key === "message" && strlen($cleanValue) > 65535) {
                 $errors[$key] = "La description est trop longue, merci de raccourcir la description";
             }
-            if($key === "adress" && strlen($cleanValue) > 255){
+            if ($key === "adress" && strlen($cleanValue) > 255) {
                 $errors[$key] = "L'adresse est trop longue, merci de donner une adresse valide";
             }
-            if($key === "city" && strlen($cleanValue) > 100){
+            if ($key === "city" && strlen($cleanValue) > 100) {
                 $errors[$key] = "Le nom de la Ville est trop long, merci d'utiliser un nom de ville valide";
             }
-            if($key === "zipcode" && strlen($cleanValue) > 5){
+            if ($key === "zipcode" && strlen($cleanValue) > 5) {
                 $errors[$key] = "Le Code Postal est trop long, merci d'utiliser un Code Postal valide";
             }
-            if($key === "zipcode" && is_int ($cleanValue)){
-                $errors[$key] = "Code Postal non valide, merci d'utiliser un Code Postal valide";
-            }
-            if($key === "email" && strlen($cleanValue) > 20){
+            if ($key === "email" && strlen($cleanValue) > 20) {
                 $errors[$key] = "L'email est trop long, merci de fournir une adresse mail plus courte";
             }
-            if($key === "email" && !filter_var($cleanValue, FILTER_VALIDATE_EMAIL)){
+            if ($key === "email" && !filter_var($cleanValue, FILTER_VALIDATE_EMAIL)) {
                 $errors[$key] = "Adresse mail non valide, merci de fournir une adresse mail valide";
             }
-            if($key === "firstname" && strlen($cleanValue) > 20){
+            if ($key === "firstname" && strlen($cleanValue) > 20) {
                 $errors[$key] = "Prénom trop long, merci de fournir un prénom plus court";
             }
-            if($key === "lastname" && strlen($cleanValue) > 20){
+            if ($key === "lastname" && strlen($cleanValue) > 20) {
                 $errors[$key] = "Nom trop long, merci de fournir un prénom plus court";
-            }
-            if(!isset($errors[$key])){
+            }*/
+            if (!isset($errors[$key])) {
                 $gooDeal[$key] = $cleanValue;
             }
         }
@@ -65,38 +62,39 @@ class FormAddGoodealController extends AbstractController
 
 
     public function addGoodeal(): string
-    {$data = [];
+    {
+        $data = [];
         $checkedData = [];
-       
-           if($_SERVER["REQUEST_METHOD"] === 'POST'){
-               $data = [
+
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+                $data = [
                 "title" => $_POST['deal-name'],
                 "lastname" => $_POST['lastname'],
                 "firstname" => $_POST['firstname'],
                 "category" => $_POST['category'],
                 "adress" => $_POST['adress'],
-                "region" =>$_POST['region'],
+                "region" => $_POST['region'],
                 "city" => $_POST['city'],
                 "zipcode" => $_POST['zip-code'],
-                "start-date"=> $_POST['start-date'],
+                "start-date" => $_POST['start-date'],
                 "end-date" => $_POST['end-date'],
-                "image" => $_POST['avatar'],
+                //"image" => $_POST['avatar'],
                 "email" => $_POST['email'],
-                "message" => $_POST['description']     
-               ];
+                "message" => $_POST['description']
+                ];
 
-               $checkedData = $this->checkForm($data);
-               
-               if(empty($checkedData["errors"])){
-                $addGoodealManager = new AddGoodealManager();
-                $addGoodealManager->addGoodeal($checkedData["gooDeal"]);
-                header('Location: /addGoodeal');
-               }
-           }
+                $checkedData = $this->checkForm($data);
+
+                if (empty($checkedData["errors"])) {
+                    $addGoodealManager = new AddGoodealManager();
+                    $addGoodealManager->insertGoodeal($checkedData["gooDeal"]);
+                    header('Location: /addGoodeal');
+                }
+        }
            // Generate the web page
-               
+
                return $this->twig->render('FormAddGoodeal/index.html.twig', [
                    'errors' => $checkedData['errors']
                ]);
-       }
     }
+}
--- src/Model/Connection.php
+++ src/Model/Connection.php
@@ -3,7 +3,6 @@
 namespace App\Model;
 
 use PDO;
-use PDOException;
 
 /**
  * This class make a PDO object instanciation.
@@ -11,31 +10,20 @@ use PDOException;
 class Connection
 {
     private PDO $connection;
-    private string $user;
-    private string $host;
-    private string $password;
-    private string $database;
+    private string $user = DB_USER;
+    private string $host = DB_HOST;
+    private string $password = DB_PASSWORD;
+    private string $database = DB_NAME;
 
     public function __construct()
     {
-        $this->user = DB_USER;
-        $this->$host = DB_HOST;
-        $this->$password = DB_PASSWORD;
-        $this->$database = DB_NAME;
-        try {
         $this->connection = new PDO(
             'mysql:host=' . $this->host . '; dbname=' . $this->database . '; charset=utf8',
             $this->user,
             $this->password
         );
+
         $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        
-        if (ENV === 'dev') {
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-    } catch (PDOException $e) {
-        echo '<div class="error">Error !: ' . $e->getMessage() . '</div>';
-    }
     }
 
     public function getconnection(): PDO
