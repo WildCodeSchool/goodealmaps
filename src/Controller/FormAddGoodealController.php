face8fffcfb0e6f0c82a5802f928c36660f0c31b
Axel Juste <ax.juste@gmail.com>
2022-11-08 11:08:24 +0100

    grump happy with form

--- src/Controller/FormAddGoodealController.php
+++ src/Controller/FormAddGoodealController.php
@@ -11,15 +11,15 @@ class FormAddGoodealController extends AbstractController
     {
         return $this->twig->render('Announcement/addGoodeal.html.twig');
     }
-/*
+
   private function cleanValue(array $data): array
-  {
+  { 
     foreach ($data as $key => $value) {
         $cleanValue = htmlentities(trim($value));
         $checkedData[$key] = $cleanValue;
     }
     return $checkedData;
-
+   
   }
 
     public function checkForm(array $data): array
@@ -28,7 +28,6 @@ class FormAddGoodealController extends AbstractController
         $gooDeal = [];
 
         foreach ($data as $key => $value) {
- //           $value = htmlentities(trim($value));
             switch ($value) {
                 case "":
                     $errors[$key] = "Merci de Remplir le champ";
@@ -40,46 +39,46 @@ class FormAddGoodealController extends AbstractController
             switch ($key) {
                 case "title":
                     if (strlen($value) > 50) {
-                        $errors[$key] = "Le nom du Goodeal est trop long, merci de donner
+                        $errors[$key] = "Le nom du Goodeal est trop long, merci de donner 
                         un nom plus court à votre Goodeal";
                     }
                     break;
                 case "lastname":
                     if (strlen($value) > 20) {
-                        $errors[$key] = "Nom trop long, merci de fournir un nom
+                        $errors[$key] = "Nom trop long, merci de fournir un nom 
                         plus court";
                     }
                     break;
                 case "firstname":
                     if (strlen($value) > 20) {
-                        $errors[$key] = "Prénom trop long, merci de fournir un prénom
+                        $errors[$key] = "Prénom trop long, merci de fournir un prénom 
                         plus court";
                     }
                     break;
                 case "email":
                     if (strlen($value) > 20) {
-                        $errors[$key] = "L'email est trop long, merci de fournir une
+                        $errors[$key] = "L'email est trop long, merci de fournir une 
                         adresse mail plus courte";
                     } elseif (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                        $errors[$key] = "Adresse mail non valide, merci de fournir
+                        $errors[$key] = "Adresse mail non valide, merci de fournir 
                         une adresse mail valide";
                     }
                     break;
                 case "adress":
                     if (strlen($value) > 255) {
-                        $errors[$key] = "L'adresse est trop longue, merci de donner
+                        $errors[$key] = "L'adresse est trop longue, merci de donner 
                         une adresse valide";
                     }
                     break;
                 case "city":
                     if (strlen($value) > 100) {
-                        $errors[$key] = "Le nom de la Ville est trop long, merci
+                        $errors[$key] = "Le nom de la Ville est trop long, merci 
                         d'utiliser un nom de ville valide";
                     }
                     break;
                 case "zipcode":
                     if (strlen($value) > 5 || strlen($value) < 5) {
-                        $errors[$key] = "Le Code Postal n'est pas valide, merci
+                        $errors[$key] = "Le Code Postal n'est pas valide, merci 
                         d'utiliser un Code Postal valide";
                     }
                     break;
@@ -89,7 +88,7 @@ class FormAddGoodealController extends AbstractController
 
           /*  if ($key === "message" && strlen($value) > 65535) {
                 $errors[$key] = "La description est trop longue, merci de raccourcir la description";
-            }
+            }*/
 
             if (empty($errors[$key])) {
                 $gooDeal[$key] = $value;
@@ -122,9 +121,9 @@ class FormAddGoodealController extends AbstractController
                 "zipcode" => $_POST['zipcode'],
               /*  "start-date" => $_POST['start-date'],
                 "end-date" => $_POST['end-date'],
-                "image" => $_POST['avatar'],
+                "image" => $_POST['avatar'],*/
                 "email" => $_POST['email'],
-           /*     "message" => $_POST['description']
+           /*     "message" => $_POST['description']*/
                 ];
 
                 $checkedData = $this->cleanValue($data);
@@ -145,5 +144,5 @@ class FormAddGoodealController extends AbstractController
             'data' => $data,
             'errors' => $finalValue['errors']
                ]);
-    }*/
+    }
 }
