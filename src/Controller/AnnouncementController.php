da02954f7b06f8b24dc193d82fe3163c3c03b102
PAUL VINCHENT <pvinchent@gmail.com>
2022-11-09 13:12:47 +0100

    problem fixed

--- src/Controller/AnnouncementController.php
+++ src/Controller/AnnouncementController.php
@@ -73,44 +73,6 @@ class AnnouncementController extends AbstractController
         {
             return $this->twig->render('Announcement/addGoodeal.html.twig');
         }
-
-
-       /* $announcementManager = new AnnouncementManager();
-        $active = 'tous';
-        $regionManager = new RegionManager();
-        $where = [];
-        $selected = '';
-        $page = 1;
-        if (!(empty($_GET))) {
-            $where = $_GET;
-            if (isset($where['page'])) {
-                unset($where['page']);
-            }
-            if (isset($where['region_id'])) {
-                $selected = $where['region_id'];
-            }
-            if (isset($where['category'])) {
-                $active = $where['category'];
-            }
-        }
-        $regions = $regionManager->select();
-        $announcements = $announcementManager->select($where);
-
-        $numrows = count($announcements);
-        $numpages = ceil($numrows / $this->perPage);
-
-        if ($numpages > 1) {
-            $page = (!isset($_GET['page']) || $_GET['page'] == 0 || $_GET['page'] > $numpages) ? 1 : $_GET['page'];
-            $begin = ($page - 1) * $this->perPage;
-            $end = $this->perPage;
-            $where['limitQuery'] = ' LIMIT ' . $begin . ',' . $end;
-            //$where['pageURL'] = '&page=' . $where['page'];
-            $announcements = $announcementManager->select($where);
-            unset($where['limitQuery']);
-        }
-        return $this->twig->render('Announcement/index.html.twig', ['announcements' => $announcements,
-        'events' => $this->events, 'active' => $active, 'regions' => $regions, 'selected' => $selected,
-        'numpages' => $numpages, 'where' => $where, 'page' => $page]);*/
     }
 
     /**
--- src/Model/AddGoodealManager.php
+++ src/Model/AddGoodealManager.php
@@ -55,14 +55,5 @@ class AddGoodealManager extends AbstractManager
 
 
         $statement->execute();
-
-/*    public function update(array $item): bool
-    {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item['id'], PDO::PARAM_INT);
-        $statement->bindValue('title', $item['title'], PDO::PARAM_STR);
-
-        return $statement->execute();
-    }*/
     }
 }
